7ec75fff831bf272bd6d3f4ddf483aa7a4f145cb
Franklin Hau <fhauchi@gmail.com>
2023-09-03 21:16:32 -0600

    Getting remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        '''Fetch the raw response content (as bytes) from the URL.'''
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        '''Fetch the response content, parse it as JSON, and return the resulting object.'''
+        response = requests.get(self.url)
+        return response.json()
